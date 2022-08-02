92612caeedcc854d5d7108eb67f049ea6f3d5284
KSukhjinder <Sukhjinder.Kaur@fullstacklabs.ca>
2022-08-02 16:54:51 -0400

    did some changes in yaml file

--- src/main/java/com/jenkins/jenkins/JenkinsApplication.java
+++ src/main/java/com/jenkins/jenkins/JenkinsApplication.java
@@ -3,13 +3,13 @@ package com.jenkins.jenkins;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class JenkinsApplication {
 
 	@Autowired
