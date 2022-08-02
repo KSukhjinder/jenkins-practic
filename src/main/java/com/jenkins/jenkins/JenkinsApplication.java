d1e24937292b7770f4e3181dbd748dbe7fa5c7fb
KSukhjinder <Sukhjinder.Kaur@fullstacklabs.ca>
2022-08-02 13:13:57 -0400

    add get mapping method

--- src/main/java/com/jenkins/jenkins/JenkinsApplication.java
+++ src/main/java/com/jenkins/jenkins/JenkinsApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,6 +14,11 @@ public class JenkinsApplication {
 
 	@Autowired
 	TableServiceImpl tableService;
+
+	@GetMapping("/")
+	public String message(){
+		return "Welcome..............";
+	}
 
 	@PostMapping("/save")
 	public TableEntity save(@RequestBody TableEntity entity ){
